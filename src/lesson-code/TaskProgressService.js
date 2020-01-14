ce5b824db9ba94bf53132d3736b0253490929a30
Rares Matei <matei.rar@gmail.com>
2020-01-14 20:50:42 +0000

    add solution

--- src/lesson-code/TaskProgressService.js
+++ src/lesson-code/TaskProgressService.js
@@ -1,1 +1,29 @@
+import { Observable, merge } from "rxjs";
+import {
+  mapTo,
+  scan,
+  startWith,
+  distinctUntilChanged,
+  shareReplay
+} from "rxjs/operators";
+
+const taskStarts = new Observable();
+const taskCompletions = new Observable();
+const displaySpinner = new Observable();
+
+const loadUp = taskStarts.pipe(mapTo(1));
+const loadDown = taskCompletions.pipe(mapTo(-1));
+
+const loadVariations = merge(loadUp, loadDown);
+
+const currentLoadCount = loadVariations.pipe(
+  scan((totalCurrentLoads, changeInLoads) => {
+    const newLoadCount = totalCurrentLoads + changeInLoads;
+    return newLoadCount > 0 ? newLoadCount : 0;
+  }, 0),
+  startWith(0),
+  distinctUntilChanged(),
+  shareReplay(1)
+);
+
 export default {};
