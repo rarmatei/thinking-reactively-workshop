c02acb30ef0ca93a1a70f4815acfb5b88ad2456a
Rares Matei <matei.rar@gmail.com>
2020-01-15 14:16:27 +0000

    add operators and bonus exercises

---
+++ src/lesson-code/teaching-eventCombo.js
@@ -0,0 +1,24 @@
+/*
+  we'll split it up into two:
+  whenever somebody starts a combo
+    keep taking(listening) for keys
+      until the timer has finished
+        explain with clock 
+      and while the combo is being followed correctly
+        show us missing the 3rd key
+      and until we've reached the end of the combo
+        the first two conditions are filters
+        we'll only get values here if the user has been correctly following the combo
+        
+
+      explain them on by one in PP
+      WE ENTER COMBO mode
+      that's why we separate them - because now we can think only about
+        what's make us exit combo mode
+
+      
+
+      
+*/
+
+// for explaining switchMap - https://stackblitz.com/edit/exhaust-map-demo?file=index.ts
